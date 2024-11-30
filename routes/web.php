89af675b1ad622f13f23ef01e77fd316279c630e
bappybejpara123 <bappyfreelancer123@gmail.com>
2024-11-30 19:32:15 +0600

    user crud

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('coming');
+});
+
+Route::prefix('users')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/create/{name}/{phone}/{pin}', [UserController::class, 'create']);
+    Route::get('/update/{name}/{phone}/{pin}/{id}', [UserController::class, 'update']);
+    Route::get('/delete/{id}', [UserController::class, 'delete']);
 });
