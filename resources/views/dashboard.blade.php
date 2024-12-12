2fc11563c9f61e15b07acc1efbadaca03efa1004
ongsho <info@ongsho.com>
2024-12-13 00:09:44 +0600

    change from server

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100 flex justify-between items-center">
-                    <h1 class="text-2xl font-bold ">Dashboard</h1>
+                    <h1 class="text-2xl font-bold ">Dashboard (data update)</h1>
                 </div>
             </div>
         </div>
